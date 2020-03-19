8a0f59f7e44b8999aa344c1ec5511ef18a0ee675
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2020-03-19 10:31:03 +0300

    Refactor tests to use given-when-then approach

--- src/test/kotlin/jetbrains/buildServer/notification/BaseSlackTestCase.kt
+++ src/test/kotlin/jetbrains/buildServer/notification/BaseSlackTestCase.kt
@@ -1,12 +1,13 @@
 package jetbrains.buildServer.notification
 
-import jetbrains.buildServer.BaseTestCase
 import jetbrains.buildServer.ExtensionHolder
+import jetbrains.buildServer.messages.DefaultMessagesInfo
 import jetbrains.buildServer.serverSide.MockServerPluginDescriptior
-import jetbrains.buildServer.serverSide.impl.BaseServerTestCase
+import jetbrains.buildServer.serverSide.SBuild
 import jetbrains.buildServer.serverSide.oauth.OAuthConnectionsManager
 import jetbrains.buildServer.users.SUser
 import org.testng.annotations.BeforeMethod
+import java.util.function.BooleanSupplier
 
 open class BaseSlackTestCase : BaseNotificationRulesTestCase() {
     protected lateinit var mySlackApi: MockSlackWebApi
@@ -17,6 +18,11 @@ open class BaseSlackTestCase : BaseNotificationRulesTestCase() {
     @BeforeMethod(alwaysRun = true)
     override fun setUp() {
         super.setUp()
+
+        setInternalProperty("teamcity.notifications.quiet.period.seconds", "0")
+        myFixture.notificationProcessor.setBuildFailingDelay(700)
+        myFixture.notificationProcessor.setCheckHangedBuildsInterval(50)
+
         val connectionManager = OAuthConnectionsManager(myFixture.getSingletonService(ExtensionHolder::class.java))
 
         val slackApiFactory = MockSlackWebApiFactory()
@@ -49,11 +55,77 @@ open class BaseSlackTestCase : BaseNotificationRulesTestCase() {
         myUser = createUser("test_user")
         myUser.setUserProperty(myDescriptor.channelProperty, "#test_channel")
         myUser.setUserProperty(myDescriptor.connectionProperty, "test_connection")
+        makeProjectAccessible(myUser, myProject.projectId)
     }
 
-    fun assertLastMessageContains(str: String) {
-        assertNotEmpty(mySlackApi.messages)
-        assertContains(mySlackApi.messages.last().text, str)
+    fun `given user is subscribed to`(vararg events: NotificationRule.Event) {
+        storeRules(myUser, myNotifier, newRule(*events))
+    }
+
+    fun `when build starts`(): SBuild = startBuild()
+    fun `when build finishes`(): SBuild {
+        startBuild()
+        return finishBuild()
+    }
+    fun `when build fails`(): SBuild {
+        return createFailedBuild()
+    }
+    fun `when build is failing`(): SBuild {
+        val build = startBuild()
+
+        myFixture.logBuildMessages(
+            build,
+            listOf(
+                DefaultMessagesInfo.createBlockStart(
+                    "test1",
+                    DefaultMessagesInfo.BLOCK_TYPE_TEST
+                )
+            )
+        )
+        myFixture.logBuildMessages(
+            build,
+            listOf(
+                DefaultMessagesInfo.createTestFailure(
+                    "test1",
+                    Exception("test1 failed")
+                )
+            )
+        )
+
+        myFixture.logBuildMessages(
+            build,
+            listOf(
+                DefaultMessagesInfo.createBlockEnd(
+                    "test1",
+                    DefaultMessagesInfo.BLOCK_TYPE_TEST
+                )
+            )
+        )
+
+
+        Thread.sleep(1500)
+
+        return build
+    }
+    fun `when build hangs`(): SBuild {
+        val build = startBuild()
+        makeProjectAccessible(myUser, myBuildType.projectId)
+        makeBuildHanging(build)
+        return build
+    }
+
+    fun `then message should contain`(vararg strs: String) {
+        waitForAssert(BooleanSupplier {
+            mySlackApi.messages.isNotEmpty()
+        }, 2000L)
+
+        for (str in strs) {
+            assertContains(mySlackApi.messages.last().text, str)
+        }
+    }
+
+    fun `then no messages should be sent`() {
+        assertEmpty(mySlackApi.messages)
     }
 
     private fun <T> Iterable<T>.last(): T {
--- src/test/kotlin/jetbrains/buildServer/notification/SlackNotifierTest.kt
+++ src/test/kotlin/jetbrains/buildServer/notification/SlackNotifierTest.kt
@@ -5,23 +5,44 @@ import org.testng.annotations.Test
 
 class SlackNotifierTest : BaseSlackTestCase() {
     @Test
+    fun `test should not send notification if user is subscribed to different event`() {
+        `given user is subscribed to`(NotificationRule.Event.BUILD_FINISHED_FAILURE)
+        `when build finishes`()
+        `then no messages should be sent`()
+    }
+
+    @Test
     fun `test should send notification about build start`() {
-        storeRules(myUser, myNotifier, newRule(NotificationRule.Event.BUILD_STARTED))
-
-        val build = startBuild()
-
-        assertLastMessageContains("start")
-        assertLastMessageContains(build.buildNumber)
+        `given user is subscribed to`(NotificationRule.Event.BUILD_STARTED)
+        val build = `when build starts`()
+        `then message should contain`("start", build.buildNumber)
     }
 
     @Test
     fun `test should send notification about build success`() {
-        storeRules(myUser, myNotifier, newRule(NotificationRule.Event.BUILD_FINISHED_SUCCESS))
+        `given user is subscribed to`(NotificationRule.Event.BUILD_FINISHED_SUCCESS)
+        val build = `when build finishes`()
+        `then message should contain`("success", build.buildNumber)
+    }
 
-        startBuild()
-        val build = finishBuild()
+    @Test
+    fun `test should send notification about build failure`() {
+        `given user is subscribed to`(NotificationRule.Event.BUILD_FINISHED_FAILURE)
+        val build = `when build fails`()
+        `then message should contain`("failed", build.buildNumber)
+    }
 
-        assertLastMessageContains("success")
-        assertLastMessageContains(build.buildNumber)
+    @Test
+    fun `test should send notification about build starting to fail`() {
+        `given user is subscribed to`(NotificationRule.Event.BUILD_FAILING)
+        val build = `when build is failing`()
+        `then message should contain`("failing", build.buildNumber)
+    }
+
+    @Test
+    fun `test should send notification about build probably hanging`() {
+        `given user is subscribed to`(NotificationRule.Event.BUILD_PROBABLY_HANGING)
+        val build = `when build hangs`()
+        `then message should contain`("hang", build.buildNumber)
     }
 }
