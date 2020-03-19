e8b69933e55db8d8733970c41b6bdd9a752dfd13
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2020-03-19 11:10:07 +0300

    Add tests for personal notifications and build feature notifications

--- src/test/kotlin/jetbrains/buildServer/notification/BaseSlackTestCase.kt
+++ src/test/kotlin/jetbrains/buildServer/notification/BaseSlackTestCase.kt
@@ -1,19 +1,23 @@
 package jetbrains.buildServer.notification
 
+import jetbrains.buildServer.BuildProblemData
 import jetbrains.buildServer.ExtensionHolder
 import jetbrains.buildServer.messages.DefaultMessagesInfo
 import jetbrains.buildServer.serverSide.MockServerPluginDescriptior
 import jetbrains.buildServer.serverSide.SBuild
+import jetbrains.buildServer.serverSide.impl.NotificationRulesConstants
 import jetbrains.buildServer.serverSide.oauth.OAuthConnectionsManager
 import jetbrains.buildServer.users.SUser
 import org.testng.annotations.BeforeMethod
+import java.util.*
 import java.util.function.BooleanSupplier
 
 open class BaseSlackTestCase : BaseNotificationRulesTestCase() {
-    protected lateinit var mySlackApi: MockSlackWebApi
-    protected lateinit var myDescriptor: SlackNotifierDescriptor
-    protected lateinit var myNotifier: SlackNotifier
-    protected lateinit var myUser: SUser
+    private lateinit var mySlackApi: MockSlackWebApi
+    private lateinit var myDescriptor: SlackNotifierDescriptor
+    private lateinit var myNotifier: SlackNotifier
+    private lateinit var myUser: SUser
+    private lateinit var myAssignerUser: SUser
 
     @BeforeMethod(alwaysRun = true)
     override fun setUp() {
@@ -56,10 +60,25 @@ open class BaseSlackTestCase : BaseNotificationRulesTestCase() {
         myUser.setUserProperty(myDescriptor.channelProperty, "#test_channel")
         myUser.setUserProperty(myDescriptor.connectionProperty, "test_connection")
         makeProjectAccessible(myUser, myProject.projectId)
+
+        myAssignerUser = createUser("investigation_assigner")
+        makeProjectAccessible(myUser, myProject.projectId)
     }
 
     fun `given user is subscribed to`(vararg events: NotificationRule.Event) {
         storeRules(myUser, myNotifier, newRule(*events))
+    }
+
+    fun `given build feature is subscribed to`(vararg events: NotificationRule.Event) {
+        myBuildType.addBuildFeature(
+            FeatureProviderNotificationRulesHolder.FEATURE_NAME,
+            mapOf(
+                "notifier" to myNotifier.notificatorType,
+                myDescriptor.channelProperty.key to "#test_channel",
+                myDescriptor.connectionProperty.key to "test_connection",
+                *(events.map { NotificationRulesConstants.getName(it) to "true" }).toTypedArray()
+            )
+        )
     }
 
     fun `when build starts`(): SBuild = startBuild()
@@ -68,6 +87,11 @@ open class BaseSlackTestCase : BaseNotificationRulesTestCase() {
         return finishBuild()
     }
     fun `when build fails`(): SBuild {
+        return createFailedBuild()
+    }
+    fun `when build newly fails`(): SBuild {
+        startBuild()
+        finishBuild()
         return createFailedBuild()
     }
     fun `when build is failing`(): SBuild {
@@ -114,6 +138,17 @@ open class BaseSlackTestCase : BaseNotificationRulesTestCase() {
         return build
     }
 
+    fun `when build problem occurs`(): SBuild {
+        return build().`in`(myBuildType).withProblem(
+            BuildProblemData.createBuildProblem("1", "TC_COMPILATION_ERROR_TYPE", "compilation error")
+        ).finish()
+    }
+
+    fun `when responsibility changes`(): SUser {
+        myBuildType.setResponsible(myUser, "will fix", myAssignerUser)
+        return myUser
+    }
+
     fun `then message should contain`(vararg strs: String) {
         waitForAssert(BooleanSupplier {
             mySlackApi.messages.isNotEmpty()
@@ -126,6 +161,20 @@ open class BaseSlackTestCase : BaseNotificationRulesTestCase() {
 
     fun `then no messages should be sent`() {
         assertEmpty(mySlackApi.messages)
+    }
+
+    infix fun <T> T.And(other: T): List<T> {
+        return listOf(this, other)
+    }
+
+    infix fun <T> List<T>.And(other: T): List<T> {
+        return this + other
+    }
+
+    fun Int.`messages should be sent`() {
+        waitForAssert(BooleanSupplier {
+            mySlackApi.messages.size == this
+        }, 2000L)
     }
 
     private fun <T> Iterable<T>.last(): T {
--- src/test/kotlin/jetbrains/buildServer/notification/SlackNotifierTest.kt
+++ src/test/kotlin/jetbrains/buildServer/notification/SlackNotifierTest.kt
@@ -2,47 +2,107 @@ package jetbrains.buildServer.notification
 
 
 import org.testng.annotations.Test
+import jetbrains.buildServer.notification.NotificationRule.Event.*
+
 
 class SlackNotifierTest : BaseSlackTestCase() {
     @Test
     fun `test should not send notification if user is subscribed to different event`() {
-        `given user is subscribed to`(NotificationRule.Event.BUILD_FINISHED_FAILURE)
+        `given user is subscribed to`(BUILD_FINISHED_FAILURE)
         `when build finishes`()
         `then no messages should be sent`()
     }
 
     @Test
     fun `test should send notification about build start`() {
-        `given user is subscribed to`(NotificationRule.Event.BUILD_STARTED)
+        `given user is subscribed to`(BUILD_STARTED)
         val build = `when build starts`()
         `then message should contain`("start", build.buildNumber)
     }
 
     @Test
     fun `test should send notification about build success`() {
-        `given user is subscribed to`(NotificationRule.Event.BUILD_FINISHED_SUCCESS)
+        `given user is subscribed to`(BUILD_FINISHED_SUCCESS)
         val build = `when build finishes`()
         `then message should contain`("success", build.buildNumber)
     }
 
     @Test
     fun `test should send notification about build failure`() {
-        `given user is subscribed to`(NotificationRule.Event.BUILD_FINISHED_FAILURE)
+        `given user is subscribed to`(BUILD_FINISHED_FAILURE)
         val build = `when build fails`()
         `then message should contain`("failed", build.buildNumber)
     }
 
     @Test
     fun `test should send notification about build starting to fail`() {
-        `given user is subscribed to`(NotificationRule.Event.BUILD_FAILING)
+        `given user is subscribed to`(BUILD_FAILING)
         val build = `when build is failing`()
         `then message should contain`("failing", build.buildNumber)
     }
 
     @Test
     fun `test should send notification about build probably hanging`() {
-        `given user is subscribed to`(NotificationRule.Event.BUILD_PROBABLY_HANGING)
+        `given user is subscribed to`(BUILD_PROBABLY_HANGING)
         val build = `when build hangs`()
         `then message should contain`("hang", build.buildNumber)
+    }
+
+    @Test
+    fun `test should send notification about build problem`() {
+        `given user is subscribed to`(NEW_BUILD_PROBLEM_OCCURRED, BUILD_FINISHED_FAILURE)
+        val build = `when build problem occurs`()
+        `then message should contain`("fail", build.buildNumber)
+    }
+
+    @Test
+    fun `test should send notification about responsibility change`() {
+        `given user is subscribed to`(RESPONSIBILITY_CHANGES)
+        `when responsibility changes`()
+        `then message should contain`("investigation") And
+            1.`messages should be sent`()
+    }
+
+    @Test
+    fun `test build feature should send notification about build start`() {
+        `given build feature is subscribed to`(BUILD_STARTED)
+        val build = `when build starts`()
+        `then message should contain`("start", build.buildNumber)
+    }
+
+
+    @Test
+    fun `test build feature should send notification about build success`() {
+        `given build feature is subscribed to`(BUILD_FINISHED_SUCCESS)
+        val build = `when build finishes`()
+        `then message should contain`("success", build.buildNumber)
+    }
+
+    @Test
+    fun `test build feature should send notification about build failure`() {
+        `given build feature is subscribed to`(BUILD_FINISHED_FAILURE)
+        val build = `when build fails`()
+        `then message should contain`("failed", build.buildNumber)
+    }
+
+    @Test
+    fun `test build feature should send notification about build starting to fail`() {
+        `given build feature is subscribed to`(BUILD_FAILING)
+        val build = `when build is failing`()
+        `then message should contain`("failing", build.buildNumber)
+    }
+
+    @Test
+    fun `test build feature should send notification about build probably hanging`() {
+        `given build feature is subscribed to`(BUILD_PROBABLY_HANGING)
+        val build = `when build hangs`()
+        `then message should contain`("hang", build.buildNumber)
+    }
+
+    @Test
+    fun `test build feature should send notification about build problem`() {
+        `given build feature is subscribed to`(NEW_BUILD_PROBLEM_OCCURRED, BUILD_FINISHED_FAILURE)
+        val build = `when build problem occurs`()
+        `then message should contain`("fail", build.buildNumber)
     }
 }
