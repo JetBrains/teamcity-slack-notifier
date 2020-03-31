385f75ba954d6f88809b09e628ff260898096b9c
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2020-03-31 10:39:29 +0300

    TW-65189 Enable Slack Notifier by default

--- src/main/kotlin/jetbrains/buildServer/notification/slackNotifier/SlackNotifierEnabled.kt
+++ src/main/kotlin/jetbrains/buildServer/notification/slackNotifier/SlackNotifierEnabled.kt
@@ -1,6 +1,5 @@
 package jetbrains.buildServer.notification.slackNotifier
 
-import jetbrains.buildServer.notification.slackNotifier.SlackNotifierProperties
 import jetbrains.buildServer.serverSide.TeamCityProperties
 import org.springframework.context.annotation.Condition
 import org.springframework.context.annotation.ConditionContext
@@ -8,6 +7,6 @@ import org.springframework.core.type.AnnotatedTypeMetadata
 
 class SlackNotifierEnabled : Condition {
     override fun matches(context: ConditionContext?, metadata: AnnotatedTypeMetadata?): Boolean {
-        return TeamCityProperties.getBoolean(SlackNotifierProperties.enable)
+        return TeamCityProperties.getBooleanOrTrue(SlackNotifierProperties.enable)
     }
 }
