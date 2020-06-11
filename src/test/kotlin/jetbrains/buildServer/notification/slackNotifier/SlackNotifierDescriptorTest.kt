9dfe578cd76130e52185c98361781f6cfb5a70a3
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2020-06-11 17:39:34 +0300

    test(notification) Add test for validating 'Maximum number of changes' property

--- src/test/kotlin/jetbrains/buildServer/notification/slackNotifier/SlackNotifierDescriptorTest.kt
+++ src/test/kotlin/jetbrains/buildServer/notification/slackNotifier/SlackNotifierDescriptorTest.kt
@@ -1,9 +1,6 @@
 package jetbrains.buildServer.notification.slackNotifier
 
-import jetbrains.buildServer.ExtensionHolder
 import jetbrains.buildServer.notification.NotificatorRegistry
-import jetbrains.buildServer.serverSide.MockServerPluginDescriptior
-import jetbrains.buildServer.serverSide.oauth.OAuthConnectionsManager
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 
@@ -14,39 +11,50 @@ class SlackNotifierDescriptorTest : BaseSlackTestCase() {
     override fun setUp() {
         super.setUp()
 
-        val connectionManager = OAuthConnectionsManager(myFixture.getSingletonService(ExtensionHolder::class.java))
         descriptor = SlackNotifierDescriptor(myFixture.getSingletonService(NotificatorRegistry::class.java))
     }
 
     @Test
-    fun `test should check for channel property`() {
+    fun `should check for channel property`() {
         assertContains(
                 descriptor.validate(emptyMap()),
                 invalidProperty(SlackProperties.channelProperty.key)
         )
         assertContains(
-            descriptor.validate(mapOf(SlackProperties.channelProperty.key to "")),
-            invalidProperty(SlackProperties.channelProperty.key)
+                descriptor.validate(mapOf(SlackProperties.channelProperty.key to "")),
+                invalidProperty(SlackProperties.channelProperty.key)
         )
         assertNotContains(
-            descriptor.validate(mapOf(SlackProperties.channelProperty.key to "#some-channel")),
+                descriptor.validate(mapOf(SlackProperties.channelProperty.key to "#some-channel")),
             invalidProperty(SlackProperties.channelProperty.key)
         )
     }
 
     @Test
-    fun `test should check for connection property`() {
+    fun `should check for connection property`() {
         assertContains(
                 descriptor.validate(emptyMap()),
-            invalidProperty(SlackProperties.connectionProperty.key)
+                invalidProperty(SlackProperties.connectionProperty.key)
         )
         assertContains(
-            descriptor.validate(mapOf(SlackProperties.connectionProperty.key to "")),
-            invalidProperty(SlackProperties.channelProperty.key)
+                descriptor.validate(mapOf(SlackProperties.connectionProperty.key to "")),
+                invalidProperty(SlackProperties.channelProperty.key)
         )
         assertNotContains(
-            descriptor.validate(mapOf(SlackProperties.connectionProperty.key to "test_connection")),
-            invalidProperty(SlackProperties.connectionProperty.key)
+                descriptor.validate(mapOf(SlackProperties.connectionProperty.key to "test_connection")),
+                invalidProperty(SlackProperties.connectionProperty.key)
+        )
+    }
+
+    @Test
+    fun `should check for maximum number of notifications property`() {
+        assertNotContains(
+                descriptor.validate(emptyMap()),
+                invalidProperty(SlackProperties.maximumNumberOfChangesProperty.key)
+        )
+        assertContains(
+                descriptor.validate(mapOf(SlackProperties.maximumNumberOfChangesProperty.key to "not a number")),
+                invalidProperty(SlackProperties.maximumNumberOfChangesProperty.key)
         )
     }
 }
