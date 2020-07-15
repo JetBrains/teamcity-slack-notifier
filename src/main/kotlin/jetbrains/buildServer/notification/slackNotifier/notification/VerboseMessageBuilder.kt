065117d056edb309dc56648f3e3f54fbbc366986
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2020-07-15 19:48:16 +0300

    fix(notification) TW-66561 Use simple text instead of block to avoid text wrapping

--- src/main/kotlin/jetbrains/buildServer/notification/slackNotifier/notification/VerboseMessageBuilder.kt
+++ src/main/kotlin/jetbrains/buildServer/notification/slackNotifier/notification/VerboseMessageBuilder.kt
@@ -22,26 +22,22 @@ class VerboseMessageBuilder(
         private val webLinks: RelativeWebLinks,
         private val notificationBuildStatusProvider: NotificationBuildStatusProvider,
         private val server: SBuildServer
-) : MessageBuilder {
+) : MessageBuilder by messageBuilder {
     private val changeDateFormat = SimpleDateFormat("d MMM HH:mm")
 
     override fun buildStarted(build: SRunningBuild): MessagePayload = messagePayload {
-        textBlock {
+        text {
             add(messageBuilder.buildStarted(build))
             addBranch(build)
             addChanges(build)
         }
-
-        addActionsBlock(build)
     }
 
     override fun buildSuccessful(build: SRunningBuild): MessagePayload = messagePayload {
-        textBlock {
+        text {
             add(messageBuilder.buildSuccessful(build))
             addVerboseInfo(build, BuildEvent.BUILD_SUCCESSFUL)
         }
-
-        addActionsBlock(build)
     }
 
     private fun MessagePayloadBlockBuilder.addVerboseInfo(build: Build, buildEvent: BuildEvent) {
@@ -103,29 +99,17 @@ class VerboseMessageBuilder(
 
             add("$changeDescription ${format.italic(changeAdditionalInfo)}")
         }
-    }
 
-    private fun MessagePayloadBuilder.addActionsBlock(build: Build) {
-        if (!verboseMessagesOptions.addChanges) {
-            return
+        newline()
+
+        val text = if (changes.size == 1) {
+            "View 1 change in TeamCity"
+        } else {
+            "View all ${changes.size} changes in TeamCity"
         }
 
-        val changes = build.getChanges(SelectPrevBuildPolicy.SINCE_LAST_BUILD, true)
-        if (changes.isEmpty()) {
-            return
-        }
-
-        actionsBlock {
-            val changesUrl = webLinks.getViewChangesUrl(build)
-
-            val text = if (changes.size == 1) {
-                "View 1 change in TeamCity"
-            } else {
-                "View ${changes.size} changes in TeamCity"
-            }
-
-            button(text = text, url = changesUrl)
-        }
+        val changesUrl = webLinks.getViewChangesUrl(build)
+        add(format.url(url = changesUrl, text = text))
     }
 
     private fun shorten(text: String, maximumLength: Int = 80): String {
@@ -140,95 +124,37 @@ class VerboseMessageBuilder(
     }
 
     override fun buildFailed(build: SRunningBuild): MessagePayload = messagePayload {
-        textBlock {
+        text  {
             add(messageBuilder.buildFailed(build))
             addVerboseInfo(build, BuildEvent.BUILD_FAILED)
         }
-
-        addActionsBlock(build)
     }
 
     override fun buildFailedToStart(build: SRunningBuild): MessagePayload = messagePayload {
-        textBlock {
+        text {
             add(messageBuilder.buildFailedToStart(build))
             addVerboseInfo(build, BuildEvent.BUILD_FAILED_TO_START)
         }
-
-        addActionsBlock(build)
     }
 
     override fun labelingFailed(build: Build, root: VcsRoot, exception: Throwable): MessagePayload = messagePayload {
-        textBlock {
+        text {
             add(messageBuilder.labelingFailed(build, root, exception))
             addVerboseInfo(build, BuildEvent.LABELING_FAILED)
         }
-
-        addActionsBlock(build)
     }
 
     override fun buildFailing(build: SRunningBuild): MessagePayload = messagePayload {
-        textBlock {
+        text {
             add(messageBuilder.buildFailing(build))
             addVerboseInfo(build, BuildEvent.BUILD_FAILING)
         }
-
-        addActionsBlock(build)
     }
 
     override fun buildProbablyHanging(build: SRunningBuild): MessagePayload = messagePayload {
-        textBlock {
+        text {
             add(messageBuilder.buildProbablyHanging(build))
             addVerboseInfo(build, BuildEvent.BUILD_PROBABLY_HANGING)
         }
-
-        addActionsBlock(build)
-    }
-
-    override fun responsibleChanged(buildType: SBuildType): MessagePayload {
-        return messageBuilder.responsibleChanged(buildType)
-    }
-
-    override fun responsibleChanged(oldValue: TestNameResponsibilityEntry?, newValue: TestNameResponsibilityEntry, project: SProject): MessagePayload {
-        return messageBuilder.responsibleChanged(oldValue, newValue, project)
-    }
-
-    override fun responsibleChanged(testNames: Collection<TestName?>, entry: ResponsibilityEntry, project: SProject): MessagePayload {
-        return messageBuilder.responsibleChanged(testNames, entry, project)
-    }
-
-    override fun responsibleAssigned(buildType: SBuildType): MessagePayload {
-        return messageBuilder.responsibleAssigned(buildType)
-    }
-
-    override fun responsibleAssigned(oldValue: TestNameResponsibilityEntry?, newValue: TestNameResponsibilityEntry, project: SProject): MessagePayload {
-        return messageBuilder.responsibleAssigned(oldValue, newValue, project)
-    }
-
-    override fun responsibleAssigned(testNames: Collection<TestName?>, entry: ResponsibilityEntry, project: SProject): MessagePayload {
-        return messageBuilder.responsibleAssigned(testNames, entry, project)
-    }
-
-    override fun buildProblemResponsibleAssigned(buildProblems: Collection<BuildProblemInfo?>, entry: ResponsibilityEntry, project: SProject): MessagePayload {
-        return messageBuilder.buildProblemResponsibleAssigned(buildProblems, entry, project)
-    }
-
-    override fun buildProblemResponsibleChanged(buildProblems: Collection<BuildProblemInfo?>, entry: ResponsibilityEntry, project: SProject): MessagePayload {
-        return messageBuilder.buildProblemResponsibleChanged(buildProblems, entry, project)
-    }
-
-    override fun testsMuted(tests: Collection<STest?>, muteInfo: MuteInfo): MessagePayload {
-        return messageBuilder.testsMuted(tests, muteInfo)
-    }
-
-    override fun testsUnmuted(tests: Collection<STest?>, muteInfo: MuteInfo, user: SUser?): MessagePayload {
-        return messageBuilder.testsUnmuted(tests, muteInfo, user)
-    }
-
-    override fun buildProblemsMuted(buildProblems: Collection<BuildProblemInfo?>, muteInfo: MuteInfo): MessagePayload {
-        return messageBuilder.buildProblemsMuted(buildProblems, muteInfo)
-    }
-
-    override fun buildProblemsUnmuted(buildProblems: Collection<BuildProblemInfo?>, muteInfo: MuteInfo, user: SUser?): MessagePayload {
-        return messageBuilder.buildProblemsUnmuted(buildProblems, muteInfo, user)
     }
 }
