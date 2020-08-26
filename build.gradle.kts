b74722eefb95d236087c13157581dda51b764e99
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2020-08-26 11:49:32 +0300

    Downgrade from Kotlin 1.4 to 1.3.72 since Kotlin 1.4 requires Java 9, but on the server Java 8 is used

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.4.0"
+    kotlin("jvm") version "1.3.72"
     id("com.github.rodm.teamcity-server") version "1.2"
     id("com.github.rodm.teamcity-environments") version "1.2"
 }
