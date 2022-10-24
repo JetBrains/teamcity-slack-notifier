216bfbd5b7bad1d41627e4e8580ae208b3871a6d
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2022-10-24 14:09:05 +0200

    TW-67037 Use `white_check_mark` instead of `heavy_check_mark` for successful build

--- src/main/kotlin/jetbrains/buildServer/notification/slackNotifier/notification/BuildEvent.kt
+++ src/main/kotlin/jetbrains/buildServer/notification/slackNotifier/notification/BuildEvent.kt
@@ -17,7 +17,7 @@
 package jetbrains.buildServer.notification.slackNotifier.notification
 
 enum class BuildEvent(val emoji: String) {
-    BUILD_SUCCESSFUL(":heavy_check_mark:"),
+    BUILD_SUCCESSFUL(":white_check_mark:"),
     BUILD_FAILED(":x:"),
     BUILD_FAILED_TO_START(":exclamation:"),
     LABELING_FAILED(":x:"),
